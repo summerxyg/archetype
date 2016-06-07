ce1cf27fb13a8e063eba20efac34bcd2f9bdb31e
summerxyg <summerxyg@sina.com>
2016-06-07 11:14:17 +0800

    change encoding configure

--- gradle-archetype/archetype-application/src/main/java/archetype/aapplication/Main.java
+++ gradle-archetype/archetype-application/src/main/java/archetype/aapplication/Main.java
@@ -2,7 +2,9 @@ package archetype.aapplication;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Hello World!");
+
+        Thread.sleep(1000 * 60 * 60l);
     }
 }
